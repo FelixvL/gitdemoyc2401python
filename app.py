bc4f0b89a83e2e43655facde8d4e5dcb50424a60
Lon Lrn <lonlrn@Lons-MacBook-Pro.local>
2024-02-23 16:18:47 +0100

    flask werkt

--- app.py
+++ app.py
@@ -1,1 +1,8 @@
 # eerste inhoud allebei iets anders felix felix &&  blablablablab xoxoxo
+from flask import Flask
+
+app = Flask(__name__)
+
+@app.route("/")
+def hello_world():
+    return "<p>Hello, World!</p>"
